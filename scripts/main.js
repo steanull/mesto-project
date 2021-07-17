c9effc5d6891fcfc12859965d662e6a1fe127b8d
steanull <onezex@gmail.com>
2021-07-17 17:10:17 +0300

    added cards from js

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,67 @@ const editProfileName = document.querySelector('.profile-info__title');
 const formElement = document.querySelector('.form');
 const nameInput = formElement.querySelector('.edit-name');
 const jobInput = formElement.querySelector('.edit-job');
+const elementsList = document.querySelector('.elements__list');
 
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+function newCard(cardData) {
+    const cardTemplate = document.querySelector('#element').content;
+    const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+    const cardTitle = cardElement.querySelector('.element__title');
+    const cardImage = cardElement.querySelector('.element__image');
+    const cardLike = cardElement.querySelector('.element__button');
+    cardTitle.textContent = cardData.name;
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    return cardElement;
+}
+
+function addCard(cardElement, cardContainer) {
+    const card = newCard(cardElement);
+    cardContainer.prepend(card);
+}
+
+initialCards.forEach ((item) => {
+    addCard(item, elementsList);
+});
 
 function openPopup(element) {
     element.classList.add('popup_opened');
@@ -32,4 +92,5 @@ function formSubmitHandler(evt) {
         closePopup(popup);
     }
 }
+
 formElement.addEventListener('submit', formSubmitHandler);
